9b241d68a7b062e15da34c1e1b0d0f5fce0ac208
assim fayas <asim1fayas@gmail.com>
2024-05-25 21:35:36 +0530

    login component validation and custom validation added

--- src/app/modules/shared/login/login.component.ts
+++ src/app/modules/shared/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../../utility/formValidations/validators';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!:FormGroup
 
+  ngOnInit(){
+   this.loginForm = new FormGroup({
+      email: new FormControl(null,[Validators.required,CustomValidators.emailPatternvalidator]),
+      password: new FormControl(null,[Validators.required,Validators.minLength(6),CustomValidators.noSpaceAllowed,CustomValidators.atLeastOneSpecialCharValidator])
+    });
+  
+  }
+
+  onLoginFormSubmitted(){
+    // if(!this.loginForm.valid){
+    //   console.log("please compleate the form");
+    //   return
+    // }else{
+    
+console.log(this.loginForm);
+// 
+  // }
+
+}
 }
--- src/app/modules/shared/register/register.component.ts
+++ src/app/modules/shared/register/register.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+registrationForm!:FormGroup
+
+ngOnInit(): void {
+  
+}
 
 }
