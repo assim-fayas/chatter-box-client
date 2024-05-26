811bde8ce8b58ac06937ae8b814925e327d97d86
assim fayas <asim1fayas@gmail.com>
2024-05-26 07:13:46 +0530

    registartion validation completed

--- src/app/modules/shared/register/register.component.ts
+++ src/app/modules/shared/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../../utility/formValidations/validators';
 
 @Component({
   selector: 'app-register',
@@ -10,8 +11,27 @@ export class RegisterComponent implements OnInit {
 
 registrationForm!:FormGroup
 
-ngOnInit(): void {
-  
+ngOnInit() {
+  this.registrationForm=new FormGroup({
+    firstName:new FormControl(null,[Validators.required,Validators.minLength(4)]),
+    lastName:new FormControl(null,[Validators.required,Validators.minLength(4)]),
+    email:new FormControl(null,[Validators.required,CustomValidators.emailPatternvalidator]),
+    password:new FormControl(null,[Validators.required,Validators.minLength(6),Validators.maxLength(12),CustomValidators.noSpaceAllowed,CustomValidators.atLeastOneSpecialCharValidator]),
+  })
 }
+
+onRegistrationFormSubmit(){
+//  if(!this.registrationForm.valid){
+//   console.log("please fill all the rewuired fields");
+  
+//  }else{
+
+//  }
+
+console.log(this.registrationForm );
+
+
+}
+
 
 }
